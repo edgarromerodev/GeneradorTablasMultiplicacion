14d02f454acb486137bdd40dabab928e5cf9836d
edgarromero1994 <edgarromerocuc94@gmail.com>
2024-08-30 21:57:24 -0600

    actualice el codigo script js

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-document.getElementById("generateButton").addEventListener("click", generarTabla);
-document.getElementById("clearButton").addEventListener("click", limpiarTabla)
+document.addEventListener("DOMContentLoaded", () => {
+    const botonGenerar = document.getElementById("botonGenerar")
+    botonGenerar.addEventListener("click", generarTabla)
 
-function generarTabla () {
-    const datosRecibidos = parseInt(document.getElementById("numberInput").value);
-    const mostrarTabla = document.getElementById("tableContainer")
-    mostrarTabla.innerHTML = "";
 
-    if (isNaN(datosRecibidos) || datosRecibidos  <= 0) {
-        alert("Por favor ingrese un número valido");
-        return;
+    function generarTabla () {
+        const datosRecibidos = document.getElementById("numberInput").value; 
+        const mostrarTabla = document.getElementById("tableContainer")
+        mostrarTabla.innerHTML = ""
+
+        if (isNaN(datosRecibidos) || datosRecibidos <= 0) {
+            alert("Por favor ingrese un número válido");
+            return;
+        }
+        //crear tabla 
+        const tabla = document.createElement("table")
+        //crear fila del encabezado
+        const filaEncabezado = tabla.insertRow()
+        //crear celdas dentro del encabezado
+        const celdasEncabezado = document.createElement("th")
+
+        celdasEncabezado.textContent = `Tabla de multiplicar del ${datosRecibidos}`   
+
+        filaEncabezado.appendChild(celdasEncabezado)
+
+        for (let i = 1; i <= 10; i++) {
+            //creamos las filas en el cuerpo de la tabla
+            const fila = tabla.insertRow()
+            //dentro de la fila insertamos las celdas 
+            const celda = fila.insertCell()
+
+            celda.textContent = `${datosRecibidos} X ${i} = ${datosRecibidos * i}`
+          
+        }
+
+        mostrarTabla.appendChild(tabla)
+
     }
 
-    //crear tabla 
-    const crearTabla = document.createElement("table");
-    //crear fila del encabezado
-    const filaDelEncabezado = crearTabla.insertRow();
-    //crear celdas dentro del encabezado
-    const celdaDelEncabezado = document.createElement("th");
 
-    celdaDelEncabezado.textContent = `Tabla de Multiplicar del ${datosRecibidos}`
-    filaDelEncabezado.appendChild(celdaDelEncabezado);
-
-    for (let i = 1; i <= 10; i++) {
-        //creamos las filas en el cuerpo de la tabla
-        const fila = crearTabla.insertRow();
-        //dentro de la fila insertamos las celdas 
-        const celda = fila.insertCell();
-        //en la celda mostramos la tabla de multiplicar
-        celda.textContent = `${datosRecibidos} X ${i} = ${datosRecibidos * i}`
-        console.log(celda)
-    }
-
-    mostrarTabla.appendChild(crearTabla);
-}
+    document.getElementById("botonLimpiar").addEventListener("click", 
+        function limpiarTabbla() {
+            document.getElementById("numberInput").value = "";
+            document.getElementById("tableContainer").innerHTML = ""
+        }
+    )
 
 
-function limpiarTabla () {
-    document.getElementById("numberInput").value = "";
-    document.getElementById("tableContainer").innerHTML = "";
-}
+})
